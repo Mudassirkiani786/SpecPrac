50815f7e72fce92cff43a446bddb355b221e76d1
Mudassirkiani786 <kianimudasir28@gmail.com>
2026-05-06 21:27:08 +0500

    Add manifest file for BoM Merged Materials module

---
+++ mrp_bom_merged_materials/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "BoM Merged Materials",
+    "version": "17.0.1.0.0",
+    "depends": ["mrp"],
+    "data": [
+        "views/mrp_bom_views.xml",
+        "views/wizard_views.xml",
+    ],
+    "installable": True,
+}
